505c2d86d225f851c810b36098a97396adbbb9f5
keroloussamy <keroloussamy98@gmail.com>
2022-07-02 21:07:15 +0200

    Add Navbar component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './home.scss'
 import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const Home = () => {
   return (
     <div className='home'>
       <Sidebar />
       <div className="homeContainer">
-
+        <Navbar />
       </div>
     </div>
   )
