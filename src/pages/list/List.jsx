18c617dbfb690e591f869eb4fa05903b64ef3fe3
keroloussamy <keroloussamy98@gmail.com>
2022-07-05 15:53:32 +0200

    Add DataTable component

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import "./list.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import DataTable from "../../components/dataTable/DataTable"
 
 const List = () => {
   return (
-    <div>List</div>
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <DataTable/>
+      </div>
+    </div>
   )
 }
 
